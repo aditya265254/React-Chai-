e9681a0c13be7bff379a2b00b71ef0554aee263a
Aditya singh <144699096+aditya265254@users.noreply.github.com>
2026-07-30 18:24:03 +0530

    feat: add initial React Router setup with components for Home, About, Contact, User, and Github
    - Created App component as entry point
    - Added About component with styled content
    - Implemented Contact component with a form and contact details
    - Developed Footer component with navigation links
    - Created Github component to fetch and display GitHub user data
    - Added Header component for navigation with links to Home, About, Contact, and Github
    - Implemented Home component with promotional content and images
    - Created User component with similar structure to Home
    - Added Tailwind CSS for styling
    - Configured Vite for React and Tailwind CSS
    - Set up routing with createBrowserRouter and defined routes for the application

---
+++ 04 reactRouter/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Header from "./src/components/Header/Header"
+import Footer from "./src/components/Footer/Footer"
+
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
